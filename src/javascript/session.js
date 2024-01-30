74132f7d2ca6794bd704b2d392a82c2b00196f15
Ryan Tan <ryantan.mail@gmail.com>
2024-01-29 18:55:21 -0800

    working timer

--- src/javascript/session.js
+++ src/javascript/session.js
@@ -1,12 +1,15 @@
-const sessionList = document.getElementById("sessionList");
 const sessionButton = document.getElementById("sessionGenerateButton");
 sessionButton.addEventListener("click", generate);
 
 function generate(){
+	const sessionList = document.getElementById("sessionList");
 	const sessions = document.getElementById("numOfSessions");
 	const sessionMinutes = document.getElementById("sessionMinutes");
 	const sessionSeconds = document.getElementById("sessionSeconds");
-		
+	if (sessionSeconds.value == "0" && sessionMinutes.value == "0"){
+		alert("At least one second per session, come on now.");
+		return;
+	}
 	for (let i = 0; i < sessions.value; i++){
 		const newSession = document.createElement("li");
 		newSession.className = "session";
@@ -15,27 +18,52 @@ function generate(){
 		const newSessionButton = document.createElement("button");
 		newSessionButton.className = "sessionClearButton";
 		newSessionButton.type = "button";
-		newSessionButton.addEventListener("click", clearTask);
+		newSessionButton.addEventListener("click", Task);
 		newSession.appendChild(newSessionButton);
 		sessionList.append(newSession);
 	}
-
-	//Clear fields
-	sessions.value = 0;
-	sessionMinutes.value = 0;
-	sessionSeconds.value = 0;
+	//Session details are now immutable.
+	sessionButton.style.display = "none";
+	sessions.setAttribute("readonly", true);
+	sessionMinutes.setAttribute("readonly", true);
+	sessionSeconds.setAttribute("readonly", true);
 }
 
+function Task(){
+	const div = this.parentElement
+	startTask(div);
+	clearTask(div);
+}
 
-
-
-
-function clearTask(){
+function clearTask(thisDiv){
 	const sessionsDone = document.getElementById("currentSessionsDone");
 	let temp = parseInt(sessionsDone.innerHTML, 10);
 	temp += 1;	
 	sessionsDone.innerHTML = temp;
-	const div = this.parentElement;
-	div.style.display = "none";
+	thisDiv.style.display = "none";
+}
+
+//Pop the first session from the list and display it.
+function startTask(thisDiv){
+	const timer = document.getElementById("currentSessionTimer");
+	const sessionMinutes = document.getElementById("sessionMinutes");
+	const sessionSeconds = document.getElementById("sessionSeconds");
+	let timeLeft = 60 * parseInt(sessionMinutes.value, 10) + parseInt(sessionSeconds.value, 10);	
+	let secLeft = timeLeft % 60;
+	let minLeft = (timeLeft - secLeft) / 60;
+	timer.innerHTML = ("00"+minLeft).slice(-2)+":"+("00"+secLeft).slice(-2);
+	
+	//Loop the countDiown per second;
+	const countDown = setInterval(function(){
+		timeLeft--;
+		let secLeft = timeLeft % 60;
+		let minLeft = (timeLeft - secLeft) / 60;
+		timer.innerHTML = ("00"+minLeft).slice(-2)+":"+("00"+secLeft).slice(-2);
+		if (timeLeft == 0){
+			clearInterval(countDown);
+			alert("Session Completed");
+		}
+	}, 1000);
+
 }
 
